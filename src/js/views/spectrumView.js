0d54f7ce89e356bec09f611ed612ca8b5bda12aa
gtz.jesus <gtz.jesus@outlook.com>
2023-03-23 14:52:06 -0600

    Full responsiveness for spectrum section🤩

--- src/js/views/spectrumView.js
+++ src/js/views/spectrumView.js
@@ -14,29 +14,32 @@ class SpectrumView {
     return `
             <div class="spectrum__container">
                 <div class="spectrum__intro">
-                    <span class="spectrum__title">selected creations</span>
+                    <span class="spectrum__title">software applications</span>
                     <span class="spectrum__subtitle">spectrum assist</span>
                 </div>
-                <div class="spectrum__display">
-                    <img src="${spectrum}" />
-                </div>
+
                 <div  class="spectrum__description">
-                <span>
-                __________a software application whose <span class="lastname__spectrum">mission is to aid</span>  spectrum technicians
-                through various troubleshooting processes.
-                </span>
-                </div>
+                  <div class="spectrum__display">
+                    <img src="${spectrum}" />
+                  </div>
+                  <div class="spectrum__body">
+                  <span class="spectrum__desc">
+                  __________a software application whose <span class="lastname__spectrum">mission is to aid</span>  spectrum technicians
+                  through various troubleshooting processes.
+                  </span>
                 <div class="spectrum__link">
-                  <a class="spectrum__link__a" href="https://spectrumassist.netlify.app" target="_blank">
-                    <div class="spectrum__link__cont">
-                      <span class="spectrum__link__flex">visit site <img src="${host}" title="Visit Site" /></span>
-                    </div>
+                <a class="spectrum__link__a" href="https://spectrumassist.netlify.app" target="_blank">
+                  <div class="spectrum__link__cont">
+                    <span class="spectrum__link__flex">visit site <img src="${host}" title="Visit Site" /></span>
+                  </div>
+                </a>
+                <a class="spectrum__link__a" href="https://github.com/gtzjesus/SpectrumAssist" target="_blank">
+                  <div class="spectrum__link__cont">
+                    <span class="spectrum__link__flex">visit git <img src="${gh}" title="Visit Repo" /></span>
+                  </div>
                   </a>
-                  <a class="spectrum__link__a" href="https://github.com/gtzjesus/SpectrumAssist" target="_blank">
-                    <div class="spectrum__link__cont">
-                      <span class="spectrum__link__flex">visit git <img src="${gh}" title="Visit Repo" /></span>
-                    </div>
-                    </a>
+              </div>
+              </div>
                 </div>
             </div>
         `;

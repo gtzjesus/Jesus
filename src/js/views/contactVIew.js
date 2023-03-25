c031b1eee41681dbd9d4c0516f5bb4ccb53e49f3
gtz.jesus <gtz.jesus@outlook.com>
2023-03-24 18:45:04 -0600

    Form implementation

--- src/js/views/contactVIew.js
+++ src/js/views/contactVIew.js
@@ -11,10 +11,21 @@ class ContactView {
         <div class="contact__container">
             <div class="contact__introduction">
                 <div class="contact__title">
-                    <span>let'ss <br /> connect</span>
+                    <span>let's <br /> connect</span>
                 </div>
                 <div class="contact__subtitle">
-                    <span>minding a project?</span>
+                    <span>what brings you here?</span>
+                </div>
+            </div>
+            <div class="contact__body">
+                <div class="contact__form">
+                    <form>
+                        <span></span>
+                        <input type="text" id="name" placeholder="NAME" required />
+                        <input type="text" id="email" placeholder="EMAIL" required />
+                        <textarea id="message" placeholder="MESSAGE" required></textarea>
+                        <button type="submit">SUBMIT</button>                        
+                    </form>
                 </div>
             </div>
         </div>

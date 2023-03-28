dceab4f39712d1fa84a8ac7b5a16737a219fbf55
gtz.jesus <gtz.jesus@outlook.com>
2023-03-28 09:57:25 -0600

    Build process animation added👀 (REVEAL ON SCROLL)

--- src/js/handlers/form.js
+++ src/js/handlers/form.js
@@ -1,20 +1,22 @@
 /**
  * 'LETS CONNECT SECTION' FUNCTION GRABS USER'S INPUT FROM INPUT AREAS (INTO OBJECT)
  * USING 'emailjs' WE CAN '.send' THE INFORMATION GRABBED FROM OUR 'params object
- * INTO OUR SERVICE PROVIDER TO DISPLAY EMAIL TEMPLATE.
+ * INTO OUR SERVICE PROVIDER TO DISPLAY EMAIL TEMPLATE. **INSTALL NPM EMAILJS PACKAGE**
  * @returns MESSAGE (CONFIRMATION OF DELIVERY)
  */
 function sendMail() {
   event.preventDefault();
+  //// OBJECT CREATED TO GRAB VALUE OF USER'S INPUT IN FORM
   const params = {
     name: document.getElementById('name').value,
     email: document.getElementById('email').value,
     message: document.getElementById('message').value,
   };
 
+  //// emailjs CREDENTIALS FOR ACCESSING SERVICE AND TEMPLATE ID's
   const serviceID = 'service_0eyhwvq';
   const templateID = 'template_1dv3qcr';
-
+  //// BUILT-IN emailjs FUNCTION TO SEND DATA
   emailjs
     .send(serviceID, templateID, params)
     .then((res) => {
--- src/js/views/codeView.js
+++ src/js/views/codeView.js
@@ -1,11 +1,3 @@
-import vs from './imgs/techs/vs.png';
-import js from './imgs/techs/js.png';
-import ny from './imgs/techs/netlify.png';
-import gh from './imgs/techs/github.png';
-import node from './imgs/techs/node.png';
-import figma from './imgs/techs/figma.png';
-import arrow from './imgs/techs/arrow.png';
-
 class CodeView {
   _parentElement = window.document.querySelector('.code');
 
@@ -16,50 +8,11 @@ class CodeView {
 
   _generateMarkup() {
     return `
-        <div class ="code__container">
-            <div class="code__introduction">
+        <div class ="code__container ">
+            <div class="code__introduction ">
                 <span >my build process<span class="lastname">usually goes as</span></span>
             </div>
-                <div class="technologies">
-                    <div class="tech">
-                        <a class="tech__container" href="https://www.figma.com/" target="_blank">
-                            <img class="icon" src="${figma}" title="Visual Studio Code" />
-                            <span class="tech__subtitle">ui design</span>
-                        </a>
-                    </div>
-                    <span></span>
-                    <div class="tech">
-                    <a class="tech__container" href="https://code.visualstudio.com" target="_blank">
-                        <img class="icon" src="${vs}" title="Visual Studio Code" />
-                        <span class="tech__subtitle">code edit</span>
-                    </a>
-                    </div>
-                    <div class="tech">
-                        <a class="tech__container" href="https://nodejs.org/en/docs" target="_blank">
-                            <img class="icon" src="${node}" title="Node JS" />
-                            <span class="tech__subtitle">backend</span>
-                        </a>
-                    </div>
-                    <div class="tech">
-                        <a class="tech__container" href="https://htmlcheatsheet.com/js/" target="_blank">
-                            <img class="icon" src="${js}" title="JavaScript" />
-                            <span class="tech__subtitle">frontend</span>
-                        </a>
-                    </div>
-                    <div class="tech">
-                        <a class="tech__container" href=" https://github.com/gtzjesus" target="_blank">
-                            <img class="icon" src="${gh}" title="GitHub" />
-                            <span class="tech__subtitle">code repo</span>
-                        </a>
-                    </div>
-                    <div class="tech">
-                        <a class="tech__container" href=" https://netlify.com" target="_blank">
-                            <img class="icon" src="${ny}" title="Netlify" />
-                            <span class="tech__subtitle">host site</span>
-                        </a>
-                </div>
-                </div>
-                        </div>
+        </div>
     `;
   }
 }

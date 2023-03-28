b510f695f27fc0f3c12757b59c1c3457b1f161bc
gtz.jesus <gtz.jesus@outlook.com>
2023-03-28 15:39:33 -0600

    Adding functionality for moble navigation💥 (BACKEND JS NAVIGATION)

--- src/js/handlers/sticky.js
+++ src/js/handlers/sticky.js
@@ -1,9 +1,11 @@
 const primaryNav = document.querySelector('.primary__navigation');
 const navToggle = document.querySelector('.mobile__nav__toggle');
+const linkOne = document.querySelector('.link-one');
+const linkTwo = document.querySelector('.link-two');
+const linkThree = document.querySelector('.link-three');
 
 navToggle.addEventListener('click', function () {
   const visibility = primaryNav.getAttribute('data-visible');
-  console.log(visibility);
 
   if (visibility === 'false') {
     primaryNav.setAttribute('data-visible', true);
@@ -12,4 +14,30 @@ navToggle.addEventListener('click', function () {
     primaryNav.setAttribute('data-visible', false);
     navToggle.setAttribute('aria-expanded', false);
   }
+  return;
+});
+
+linkOne.addEventListener('click', function () {
+  const visibility = linkOne.getAttribute('data-visible');
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+  return;
+});
+linkTwo.addEventListener('click', function () {
+  const visibility = linkTwo.getAttribute('data-visible');
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+  return;
+});
+linkThree.addEventListener('click', function () {
+  const visibility = linkThree.getAttribute('data-visible');
+  if (visibility === 'false') {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.setAttribute('aria-expanded', false);
+  }
+  return;
 });

d3fe8078abea59ec9f06440b5225a9ecf59eac37
gtz.jesus <gtz.jesus@outlook.com>
2023-04-13 09:44:34 -0600

    Primrose frontend section created👗

--- src/js/views/primroseView.js
+++ src/js/views/primroseView.js
@@ -1,6 +1,10 @@
 import primrose from './imgs/primrose/primrose.gif';
 import gh from './imgs/techs/github-visit.gif';
 import host from './imgs/techs/host.gif';
+import js from './imgs/tools/js.png';
+import node from './imgs/tools/node.png';
+import express from './imgs/tools/express.png';
+import mongo from './imgs/tools/mongo.png';
 
 class PrimroseView {
   _parentElement = document.querySelector('.primrose');
@@ -14,31 +18,40 @@ class PrimroseView {
     return `
     <div class="primrose__container">
 
-    <div class="primrose__intro">
-        <span class="primrose__subtitle">primrose</span>
-    </div>
+        <div class="primrose__intro">
+            <span class="primrose__subtitle">primrose</span>
+        </div>
 
-    <div  class="primrose__description">
-      <div class="primrose__display">
-        <img src="${primrose}" />
-      </div>
-      <div class="primrose__body">
-      <span class="primrose__desc">
-      __________an e - commerce application that allows clients to purchase trending women's clothing, jewelry, and accessories. shop today.
-      </span>
-    <div class="primrose__link">
-    <a class="primrose__link__a" href="" target="_blank">
-      <div class="primrose__link__cont">
-        <span class="primrose__link__flex">website <img src="${host}" title="Visit Site" /></span>
-      </div>
-    </a>
-    <a class="primrose__link__a" href="" target="_blank">
-      <div class="primrose__link__cont">
-        <span class="primrose__link__flex">github <img src="${gh}" title="Visit Repo" /></span>
-      </div>
-      </a>
-  </div>
-  </div>
+        <div  class="primrose__description">
+            <div class="primrose__display">
+                <img src="${primrose}" />
+            </div>
+            <div class="primrose__body">
+                <span class="primrose__desc">
+                __________an e - commerce application that allows clients to purchase trending women's clothing, jewelry, and accessories. shop today.
+                </span>
+                <div class="tools__container">
+                    <ul class="tools">
+                        <li><img src="${js}"<li>
+                        <li><img src="${node}"<li>
+                        <li><img src="${express}"<li>
+                        <li><img src="${mongo}"<li>
+
+                    </ul>
+                </div>
+            <div class="primrose__link">
+                <a class="primrose__link__a" href="" target="_blank">
+                    <div class="primrose__link__cont">
+                        <span class="primrose__link__flex">website <img src="${host}" title="Visit Site" /></span>
+                    </div>
+                </a>
+                <a class="primrose__link__a" href="" target="_blank">
+                    <div class="primrose__link__cont">
+                        <span class="primrose__link__flex">github <img src="${gh}" title="Visit Repo" /></span>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
     `;

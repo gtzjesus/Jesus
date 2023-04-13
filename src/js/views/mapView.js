3ba6d530b6818904d81a993bf1fb4e296b4f95be
gtz.jesus <gtz.jesus@outlook.com>
2023-04-13 09:59:49 -0600

    Tools created for projects

--- src/js/views/mapView.js
+++ src/js/views/mapView.js
@@ -1,6 +1,10 @@
 import map from './imgs/spectrum/mapper.gif';
 import gh from './imgs/techs/github-visit.gif';
 import host from './imgs/techs/host.gif';
+import js from './imgs/tools/js.png';
+import node from './imgs/tools/node.png';
+import express from './imgs/tools/express.png';
+import mongo from './imgs/tools/mongo.png';
 
 class MapView {
   _parentElement = window.document.querySelector('.map');
@@ -26,6 +30,15 @@ class MapView {
       <span class="map__desc">
       __________A FITNESS APPLICATION THAT <span class="lastname__map">ALLOWS USERS</span> TO LOG and keep track of MULTIPLE SETS OF CARDIOVASCULAR excercises.
       </span>
+      <div class="tools__container">
+      <ul class="tools">
+          <li ><img class="tool" src="${js}"<li>
+          <li><img class="tool" src="${node}"<li>
+          <li><img class="tool" src="${express}"<li>
+          <li><img class="tool" src="${mongo}"<li>
+
+      </ul>
+  </div>
     <div class="map__link">
     <a class="map__link__a" href="" target="_blank">
       <div class="map__link__cont">

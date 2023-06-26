5eda61f3df5a328fdb2e3dc52d18b99b3fe8b38e
gtz.jesus <gtz.jesus@outlook.com>
2023-06-26 14:56:14 -0600

    Research New Section Implemented

--- src/js/views/researchView.js
+++ src/js/views/researchView.js
@@ -16,9 +16,9 @@ class ResearchView {
           </div>
           <div class="research__sea__container">
             <div class="research__sea">
-              <span>privileged to <span class="collab">collaborate</span> with both, <span class="engineer">engineers</span> and <span class="usa">military</span> stakeholders, to fully implement <span class="sea">SEA (Scanning and Enumeration Asset)</span> used to identify <span class="critical">critical</span> cyber vulnerabilities.</span>
+              <span>privileged to <span class="collab">collaborate</span> with both, <span class="engineer">engineers</span> and <span class="usa">military</span> stakeholders, to fully implement <span class="sea">SEA (Scanning and Enumeration Asset)</span> used to identify <span class="critical">critical</span> cyber attacks.</span>
               <div class="research__click">
-                <a class="research__click__button" href="https://github.com/gtzjesus/SEA" target="blank">click me for more</a>
+                <a class="research__click__button" href="https://github.com/gtzjesus/SEA" target="blank">Learn more</a>
               </div>
             </div>
 
